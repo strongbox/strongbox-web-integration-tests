ba2060c00f23bf5cda4be6934d910494860cfdb3
Martin Todorov <carlspring@gmail.com>
2015-10-13 10:53:07 +0100

    #SB-390: Extract the integration tests into a separate module which can be executed against any URL
    Removed some code which was just meant to be for local testing, but ended up getting pushed by mistake.

--- src/it/deploy-with-valid-user/verify.groovy
+++ src/it/deploy-with-valid-user/verify.groovy
@@ -1,12 +1,6 @@
 import org.carlspring.maven.commons.util.ArtifactUtils
 import org.carlspring.strongbox.client.ArtifactClient
 
-System.out.print();
-System.out.print("**********************************");
-System.out.print(System.getProperty("strongbox.host"));
-System.out.print("**********************************");
-System.out.print();
-
 def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-project:1.0.6");
 
 def client = new ArtifactClient();

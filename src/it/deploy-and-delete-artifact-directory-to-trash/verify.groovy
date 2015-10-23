7fc68867e6f6fa3ef719316f4a5413dbffbb1e9f
Martin Todorov <carlspring@gmail.com>
2015-10-23 23:25:47 +0100

    #SB-399: Deleting directories seems to be broken
    There was no actual issue with the deleting. However, the integration tests were not exactly right and had had to be re-worked.

--- src/it/deploy-and-delete-artifact-directory-to-trash/verify.groovy
+++ src/it/deploy-and-delete-artifact-directory-to-trash/verify.groovy
@@ -5,13 +5,4 @@ def client = ArtifactClient.testInstance;
 
 client.delete("storage0", "releases-with-trash", "org/carlspring/maven/test-project/1.0.3");
 
-def artifactFile = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                            "/storage0/releases-with-trash/" +
-                            "org/carlspring/maven/test-project/1.0.3").getAbsoluteFile();
-
-def artifactFileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                                   "/storage0/releases-with-trash/.trash/" +
-                                   "org/carlspring/maven/test-project/1.0.3/test-project-1.0.3.jar").getAbsoluteFile();
-
-return !client.pathExists("storage0/releases-with-trash/org/carlspring/maven/test-project/1.0.3") && !artifactFile.exists() &&
-       artifactFileInTrash.exists();
+return !client.pathExists("storages/storage0/releases-with-trash/org/carlspring/maven/test-project/1.0.3");
--- src/it/deploy-and-delete-to-trash/verify.groovy
+++ src/it/deploy-and-delete-to-trash/verify.groovy
@@ -8,21 +8,7 @@ def client = ArtifactClient.testInstance;
 
 client.deleteArtifact(artifact, "storage0", "releases-with-trash");
 
-def artifactFile = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                            "/storages/storage0/releases-with-trash/" +
-                            "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar").getAbsoluteFile();
-
-def artifactFileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                                   "/storage0/releases-with-trash/.trash/" +
-                                   "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar").getAbsoluteFile();
-
-def artifactMd5FileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                                      "/storage0/releases-with-trash/.trash/" +
-                                      "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.md5").getAbsoluteFile();
-
-def artifactSha1FileInTrash = new File(System.getProperty("strongbox.storage.booter.basedir") +
-                                       "/storage0/releases-with-trash/.trash/" +
-                                       "org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.sha1").getAbsoluteFile();
-
-return !client.artifactExists(artifact, "storage0", "releases-with-trash") && !artifactFile.exists() &&
-       artifactFileInTrash.exists() && artifactMd5FileInTrash.exists() && artifactSha1FileInTrash.exists();
+return !client.pathExists("storages/storage0/releases-with-trash/org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar") &&
+       !client.pathExists("storages/storage0/releases-with-trash/org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.md5") &&
+       client.pathExists("storages/storage0/releases-with-trash/.trash/org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar") &&
+       client.pathExists("storages/storage0/releases-with-trash/.trash/org/carlspring/maven/test-project/1.0.5/test-project-1.0.5.jar.md5");

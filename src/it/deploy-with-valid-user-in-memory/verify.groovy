88ae3071abcb041fbd9fa1bf5f0f6c11667fd89b
carlspring <carlspring@gmail.com>
2016-03-18 02:56:25 +0000

    SB-290: Remove the strongbox-annotations module
    Removed the associated tests.

--- src/it/deploy-with-valid-user-in-memory/verify.groovy
+++
@@ -1,9 +0,0 @@
-import org.carlspring.maven.commons.util.ArtifactUtils
-import org.carlspring.strongbox.client.ArtifactClient
-
-
-def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-project:1.0.7");
-
-def client = ArtifactClient.testInstance;
-
-return client.artifactExists(artifact, "storage0", "releases-in-memory");

cd84eb50381c7329be64014d6bdd74d54f065b12
carlspring <carlspring@gmail.com>
2017-07-06 20:08:22 +0100

    SB-747: Fix the failing redeploy-release-artifact-allowed integration test
    Fixed the deploy-and-delete-to-trash test.

--- src/it/deploy-and-delete-to-trash/clean.groovy
+++ src/it/deploy-and-delete-to-trash/clean.groovy
@@ -1,14 +1,73 @@
+import org.carlspring.strongbox.client.RestClient
+import org.carlspring.strongbox.storage.repository.Repository
+import org.carlspring.strongbox.storage.repository.RepositoryLayoutEnum
+import org.carlspring.strongbox.storage.repository.RepositoryPolicyEnum
+
 import org.carlspring.maven.commons.util.ArtifactUtils
-import org.carlspring.strongbox.client.ArtifactClient
 
+def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-project:1.0.5")
 
-def artifact = ArtifactUtils.getArtifactFromGAV("org.carlspring.maven:test-project:1.0.5");
+def client = RestClient.getTestInstanceLoggedInAsAdmin()
 
-def client = ArtifactClient.testInstance;
+System.out.println()
+System.out.println()
+System.out.println("Host name: " + client.getHost())
+System.out.println("Username:  " + client.getUsername())
+System.out.println("Password:  " + client.getPassword())
+System.out.println()
+System.out.println()
+
+try
+{
+    def configuration = client.getConfiguration()
+
+    System.out.println()
+    System.out.println("configuration = " + configuration)
+    System.out.println()
+
+    def storage = configuration.getStorage("storage0")
+
+    System.out.println()
+    System.out.println("storage = " + storage.toString())
+    System.out.println()
+
+    def repositoryId = "releases-with-trash"
+
+    if (storage.getRepository(repositoryId) == null)
+    {
+
+        System.out.println()
+        System.out.println()
+        System.out.println("Creating a new repository with ID '" + repositoryId + "'...")
+        System.out.println()
+        System.out.println()
+
+        // Create the test repository:
+        def repository = new Repository(repositoryId)
+        repository.setLayout(RepositoryLayoutEnum.MAVEN_2.layout)
+        repository.setPolicy(RepositoryPolicyEnum.RELEASE.policy)
+        repository.setTrashEnabled(true)
+        repository.setStorage(storage)
+
+        client.addRepository(repository)
+
+        System.out.println()
+        System.out.println()
+        System.out.println("Repository '" + repositoryId + "' successfully created!")
+        System.out.println()
+        System.out.println()
+    }
+}
+catch (Exception e)
+{
+    e.printStackTrace()
+    return false
+}
 
 if (client.artifactExists(artifact, "storage0", "releases-with-trash"))
 {
-    client.delete("storage0", "releases-with-trash", "org/carlspring/maven/test-project/1.0.5", true);
+    client.delete("storage0", "releases-with-trash", "org/carlspring/maven/test-project/1.0.5", true)
 }
 
-client.deleteTrash("storage0", "releases-with-trash");
+client.deleteTrash("storage0", "releases-with-trash")
+

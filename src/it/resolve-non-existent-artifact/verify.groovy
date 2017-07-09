ad109d844733c92caa6e9f7e6b2931145a47b6b4
carlspring <carlspring@gmail.com>
2017-07-10 00:53:26 +0100

    SB-331: Add a maven invoker test case for missing artifacts
    Implemented.
    
    Note: This can't be done at a POM-level because the settings.xml file for the Maven invoker tests is pointing to the local file system and, for the sake of quicker testing, it's not actually hitting a remote repository for resolving artifacts, (it's only hitting it when deploying them).

---
+++ src/it/resolve-non-existent-artifact/verify.groovy
@@ -0,0 +1,23 @@
+import org.carlspring.strongbox.client.ArtifactClient
+
+
+def client = ArtifactClient.getTestInstance()
+
+System.out.println()
+System.out.println()
+System.out.println(client.getHost())
+System.out.println(client.getUsername())
+System.out.println(client.getPassword())
+System.out.println()
+System.out.println()
+
+def response = client.pathExists("/storages/storage0/snapshots/org/carlspring/strongbox/test/non-existent/" +
+                                 "1.0-SNAPSHOT/non-existent-1.0-SNAPSHOT.jar")
+
+System.out.println()
+System.out.println()
+System.out.println("Path exists? " + response.toString())
+System.out.println()
+System.out.println()
+
+return !response

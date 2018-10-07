a3176687833a11157c54df9c20dbca1832861c6e
v3rtumnus <mistrasser1@gmx.at>
2018-10-07 23:12:30 +0200

    remove obsolete tests

--- maven/src/it/deploy-and-delete-artifact-directory-to-trash/verify.groovy
+++
@@ -1,8 +0,0 @@
-import org.carlspring.strongbox.client.ArtifactClient
-
-
-def client = ArtifactClient.testInstance;
-
-client.delete("storage0", "releases-with-trash", "org/carlspring/maven/test-project/1.0.3");
-
-return !client.pathExists("storages/storage0/releases-with-trash/org/carlspring/maven/test-project/1.0.3");

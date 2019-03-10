0519913c58c2dee2f1d1206616aa526131d6a430
carlspring <carlspring@gmail.com>
2019-03-10 22:51:29 +0000

    Groovy configuration polishing up.

--- gradle/src/it/common-flow/test-gradle-common-flow.groovy
+++ gradle/src/it/common-flow/test-gradle-common-flow.groovy
@@ -10,7 +10,7 @@ def gradlePath = getGradlePath(project)
 def executionPath = gradlePath.resolve('src').resolve('it').resolve('common-flow')
 
 
-def gradlewName;
+def gradlewName
 
 // Determine OS and appropriate gradlew executable
 if (System.getProperty("os.name").toLowerCase().contains("windows")) {
@@ -19,7 +19,7 @@ if (System.getProperty("os.name").toLowerCase().contains("windows")) {
     gradlewName = "gradlew"
 }
 
-def gradleExec = gradlePath.resolve(gradlewName).toString();
+def gradleExec = gradlePath.resolve(gradlewName).toString()
 
 def username = "maven"
 def password = "password"
@@ -27,12 +27,12 @@ def password = "password"
 System.out.println(gradleExec)
 
 runCommand(executionPath, String.format("$gradleExec upload -Dcredentials.username=%s -Dcredentials.password=%s",
-                                    username,
-                                    password))
+                                        username,
+                                        password))
 
 // check if artifact was uploaded to Strongbox
 assert targetPath.resolve('strongbox-vault/storages/storage0/snapshots/org/carlspring/strongbox/' +
-                         'examples/hello-strongbox-gradle/1.0-SNAPSHOT')
-                         .resolve('hello-strongbox-gradle-1.0-SNAPSHOT.jar')
-                         .toFile().exists()
+                          'examples/hello-strongbox-gradle/1.0-SNAPSHOT')
+                 .resolve('hello-strongbox-gradle-1.0-SNAPSHOT.jar')
+                 .toFile().exists()
 

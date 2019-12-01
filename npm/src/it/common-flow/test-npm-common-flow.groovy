f6fcd8cceaf66569eacb28d981886714febc0811
mpoznyak <mmpoznyak@gmail.com>
2019-12-02 00:34:33 +0300

    Issue-1543 add tests for npm unpublish cases

--- npm/src/it/common-flow/test-npm-common-flow.groovy
+++ npm/src/it/common-flow/test-npm-common-flow.groovy
@@ -8,6 +8,8 @@ println "Test test-npm-common-flow.groovy" + "\n\n"
 def npmExec
 def publishSuccessMsg = "+ @strongbox/npm-transitive-dependency@1.0.0"
 def resolveFailureMsg = "npm ERR! missing: @strongbox/npm-transitive-dependency"
+def unpublishVersionSuccessMsg = "- @strongbox/unpublish-test@1.0.0"
+def unpublishPackageSuccessMsg = "- @strongbox/unpublish-test"
 
 // Determine OS and appropriate commands
 if (System.getProperty("os.name").toLowerCase().contains("windows")) {
@@ -20,6 +22,9 @@ def executionPath = getExecutionPathRoot(project)
 def commandOutput
 
 def transitiveDependencyRoot = executionPath.resolve("npm-transitive-dependency")
+
+def unpublishFirstVersionRoot = executionPath.resolve("npm-unpublish-test/first-version-test")
+def unpublishSecondVersionRoot = executionPath.resolve("npm-unpublish-test/second-version-test")
 
 // Publish package to Strongbox and check output for success
 commandOutput = runCommand(transitiveDependencyRoot, npmExec + "publish")
@@ -34,3 +39,16 @@ runCommand(dependencyTestRoot, npmExec + "install")
 commandOutput = runCommand(dependencyTestRoot, npmExec + "ls")
 
 assert !commandOutput.contains(resolveFailureMsg)
+
+
+//publish versions for unpublish test
+runCommand(unpublishFirstVersionRoot, npmExec + "publish")
+runCommand(unpublishSecondVersionRoot, npmExec + "publish")
+
+//unpublish single version
+commandOutput = runCommand(transitiveDependencyRoot, npmExec + "unpublish @strongbox/unpublish-test@1.0.0")
+assert commandOutput.contains(unpublishVersionSuccessMsg)
+
+//unpublish package
+commandOutput = runCommand(transitiveDependencyRoot, npmExec + "unpublish @strongbox/unpublish-test --force")
+assert commandOutput.contains(unpublishPackageSuccessMsg)
